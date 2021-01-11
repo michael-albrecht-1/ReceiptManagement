5bfca7869749d06f5b41579c7dd5c12b8f62586b
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-01-11 20:43:29 +0100

    fix navbar responsible bug

--- php/inc/header.tpl.php
+++ php/inc/header.tpl.php
@@ -17,31 +17,35 @@
             if (isset($_SESSION['username'])):
         ?>       
              <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div class="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Liste des tickets
-                                <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="receipt.php">Ajout d'un ticket</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="export.php">Export</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['username']; ?></a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="logout.php">Déconnexion</a>
-                                <?php if ($_SESSION['username'] == "admin"): ?>
-                                    <a class="dropdown-item" href="register.php">Créer un nouveau compte</a>
-                                <?php endif ?>
-                            </div>
-                        </li>
-                    </ul>
+                <div class="container-fluid">
+            
+                    <a class="navbar-brand" href="index.php">Liste des tickets</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="receipt.php">Ajout d'un ticket</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="export.php">Export</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['username']; ?></a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="logout.php">Déconnexion</a>
+                                    <?php if ($_SESSION['username'] == "admin"): ?>
+                                        <a class="dropdown-item" href="register.php">Créer un nouveau compte</a>
+                                    <?php endif ?>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>   
+
+
 
             
         <?php endif ?>
