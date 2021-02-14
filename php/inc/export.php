a651b51e2631720ee20ca1de902d1c9ca346e29b
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-02-14 20:15:09 +0100

    export xls ok

--- php/inc/export.php
+++ php/inc/export.php
@@ -1,49 +1,95 @@
     <?php 
         session_start();
-        $excel = "\ndate_emission\tjournal_achat\t\t\tfournisseur\tcredit\tdebit\n";
+
+        $receiptNumber = 1;
+        
+
+
+        $excel = "\ndate_emission\tjournal_achat\tcompte\tnumero\tfournisseur\tcredit\tdebit\n";
         
         foreach($_SESSION['receiptsToExport'] as $row) {
-            
-            switch ($row['category']) {
-                case '0':
-                    $category = '0RESTA';
+            $date_emission = date("m", strtotime($row['date_emission']));
+            $optionnalZero = $receiptNumber > 9 ? '' : '0';
+            $pieceNumber = $optionnalZero . $receiptNumber++ . '.' . $date_emission;
+
+            switch ($row['tva']) {
+                case 'tva1':
+                    $tauxTVA = 0;
                     break;
-                case '1':
-                    $category = '0CARBU';
+                case 'tva2':
+                    $tauxTVA = 5.5;
                     break;
-                case '2':
-                    $category = 'hôtel';
+                case 'tva3':
+                    $tauxTVA = 10;
                     break;
-                case '3':
-                    $category = 'péage';
+                case 'tva4':
+                    $tauxTVA = 20;
                     break;
-                case '4':
-                    $category = 'autre';
+                
+                default:
+                    $tauxTVA = 0;
                     break;
             }
-            
+
+            $tvaAmount = $row['montant_ttc'] * $tauxTVA / 100;
+            $credit = $row['montant_ttc'] - $tvaAmount;
+           
+            switch ($row['category']) {
+                case '0':
+                    $providerAccount = '0RESTA';
+                    $companyAccount = '625600';
+                    break;
+                case '1':
+                    $providerAccount = '0CARBU';
+                    $companyAccount = '606140';
+                    break;
+                case '2':
+                    $providerAccount = 'HOTEL';
+                    $companyAccount = '';
+                    break;
+                case '3':
+                    $providerAccount = 'PEAGE';
+                    $companyAccount = '';
+                    break;
+                case '4':
+                    $providerAccount = 'AUTRE';
+                    $companyAccount = '';
+                    break;
+            }
+
+            //! TTC            
             $excel .= 
-                $row['date_emission'] . "\t" .              // row 1 col1
-                "ac\t" .                                    // col2
-                "" . "\t" .                          // col3
-                "" . "\t" .                        // col4
-                $row['provider'] . "\t" .                   // col5
-                "" . "\t" .                // col6
-                "" . "\n" .                    // col7
-                $row['date_emission'] . "\t" .              // row2
-                "ac\t" .                                    // col2
-                "" . "\t" .                          // col3
-                "" . "\t" .                        // col4
-                $row['provider'] . "\t" .                   // col5
-                "" . "\t" .                // col6
-                "" . "\n" .                    // col7
-                $row['date_emission'] . "\t" .              // row3 col1
-                "ac\t" .                                    // col2
-                "" . "\t" .                          // col3
-                "" . "\t" .                        // col4
-                $row['provider'] . "\t" .                   // col5
-                "" . "\t" .                // col6
-                "" . "\n";                    // col7
+                $row['date_emission'] . "\t" .             
+                "ac\t" .                                 
+                $providerAccount . "\t" .                         
+                $pieceNumber . "\t" .                        
+                $row['provider'] . "\t" .                   
+                "0" . "\t" .              
+                $row['montant_ttc'] . "\n";   
+
+            //! TVA if the receipt isnt EXO
+            if ($row['tva'] != 'tva1') {           
+                $excel  .=                    
+                    $row['date_emission'] . "\t" .              
+                    "ac\t" .                                  
+                    "44566" . "\t" .                        
+                    $pieceNumber . "\t" .                       
+                    $row['provider'] . "\t" .                  
+                    $tvaAmount . "\t" .             
+                    "0" . "\n";                    
+                } 
+            
+            // ! HT
+            $excel .=
+                $row['date_emission'] . "\t" .             
+                "ac\t" .                                   
+                $companyAccount . "\t" .                         
+                $pieceNumber . "\t" .                     
+                $row['provider'] . "\t" .                 
+                $credit . "\t" .              
+                "0" . "\n";
+             
+
         }
         
         header("Content-type: application/vnd.ms-excel");
