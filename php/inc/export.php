22b815ba26461384c9352b04d16201db91fda9a8
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-02-14 17:15:00 +0100

    build export xms

--- php/inc/export.php
+++ php/inc/export.php
@@ -1,11 +1,49 @@
     <?php 
         session_start();
-        $excel = "";
-        $excel .=  "Id\tDate\tCategorie\tTVA\tFournisseur\tMontantTTC\tPointé\tDescription\n";
+        $excel = "\ndate_emission\tjournal_achat\t\t\tfournisseur\tcredit\tdebit\n";
         
         foreach($_SESSION['receiptsToExport'] as $row) {
-            $excel .= "1\n";
-            // $excel .= "$row['id']\t$row['date_emission']\t$row['category']\t$row['tva']\t$row['provider']\t$row['montant_ttc']\t$row['checked']\t$row['description']\n";
+            
+            switch ($row['category']) {
+                case '0':
+                    $category = '0RESTA';
+                    break;
+                case '1':
+                    $category = '0CARBU';
+                    break;
+                case '2':
+                    $category = 'hôtel';
+                    break;
+                case '3':
+                    $category = 'péage';
+                    break;
+                case '4':
+                    $category = 'autre';
+                    break;
+            }
+            
+            $excel .= 
+                $row['date_emission'] . "\t" .              // row 1 col1
+                "ac\t" .                                    // col2
+                "" . "\t" .                          // col3
+                "" . "\t" .                        // col4
+                $row['provider'] . "\t" .                   // col5
+                "" . "\t" .                // col6
+                "" . "\n" .                    // col7
+                $row['date_emission'] . "\t" .              // row2
+                "ac\t" .                                    // col2
+                "" . "\t" .                          // col3
+                "" . "\t" .                        // col4
+                $row['provider'] . "\t" .                   // col5
+                "" . "\t" .                // col6
+                "" . "\n" .                    // col7
+                $row['date_emission'] . "\t" .              // row3 col1
+                "ac\t" .                                    // col2
+                "" . "\t" .                          // col3
+                "" . "\t" .                        // col4
+                $row['provider'] . "\t" .                   // col5
+                "" . "\t" .                // col6
+                "" . "\n";                    // col7
         }
         
         header("Content-type: application/vnd.ms-excel");
